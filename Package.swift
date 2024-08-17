b16749ac8b0133244c9d5499cf7850d879d86ed0
Daniel Tull <dt@danieltull.co.uk>
2024-08-17 15:00:14 +0100

    Update package file

--- Package.swift
+++ Package.swift
@@ -1,21 +1,15 @@
-// swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 5.10
 
 import PackageDescription
 
 let package = Package(
-    name: "TerminalUI",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "TerminalUI",
-            targets: ["TerminalUI"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "TerminalUI"),
+  name: "TerminalUI",
+  products: [
+    .library(name: "TerminalUI", targets: ["TerminalUI"]),
+  ],
+  targets: [
+    .target(
+      name: "TerminalUI"),
 
-    ]
+  ]
 )
