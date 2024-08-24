fbb7c2ae847f8b44383892b7944d81d507c7c036
Daniel Tull <dt@danieltull.co.uk>
2024-08-24 11:14:19 +0100

    Add a function to View to update

--- Sources/TerminalUI/Internal/BuiltinView.swift
+++ Sources/TerminalUI/Internal/BuiltinView.swift
@@ -1,5 +1,15 @@
 
 struct BuiltinView {
+
+  private let update: (Canvas) -> Void
+
+  init(update: @escaping (Canvas) -> Void) {
+    self.update = update
+  }
+
+  func update(canvas: Canvas) {
+    update(canvas)
+  }
 }
 
 extension BuiltinView: View {
--- Sources/TerminalUI/View.swift
+++ Sources/TerminalUI/View.swift
@@ -7,3 +7,15 @@ public protocol View {
   /// The content and behavior of the view.
   var body: Body { get }
 }
+
+extension View {
+
+  func update(canvas: Canvas) {
+
+    if let builtin = self as? BuiltinView {
+      builtin.update(canvas: canvas)
+    } else {
+      body.update(canvas: canvas)
+    }
+  }
+}
