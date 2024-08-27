31648839491d5ef8401ea41565cd748aa8653ea1
Daniel Tull <dt@danieltull.co.uk>
2024-08-27 15:20:11 +0200

    Convert Canvas to a struct
    This will allow us to write tests around the Canvas.

--- Sources/TerminalUI/Internal/Canvas.swift
+++ Sources/TerminalUI/Internal/Canvas.swift
@@ -1,23 +1,32 @@
 
-class Canvas {
+struct Canvas {
 
-  private var cursor: Cursor
-
-  init() {
-    self.cursor = Cursor()
+  private let draw: (Pixel, Position) -> Void
+  init(draw: @escaping (Pixel, Position) -> Void) {
+    self.draw = draw
   }
 
   func draw(_ pixel: Pixel, at position: Position) {
-    cursor.move(to: position)
-    cursor.setForegroundColor(pixel.foreground)
-    cursor.setBackgroundColor(pixel.background)
-    cursor.setBold(pixel.bold)
-    cursor.setItalic(pixel.italic)
-    cursor.setUnderline(pixel.underline)
-    cursor.setBlinking(pixel.blinking)
-    cursor.setInverse(pixel.inverse)
-    cursor.setHidden(pixel.hidden)
-    cursor.setStrikethrough(pixel.strikethrough)
-    cursor.write(pixel.content)
+    draw(pixel, position)
+  }
+}
+
+extension Canvas {
+
+  init() {
+    var cursor = Cursor()
+    self.init { pixel, position in
+      cursor.move(to: position)
+      cursor.setForegroundColor(pixel.foreground)
+      cursor.setBackgroundColor(pixel.background)
+      cursor.setBold(pixel.bold)
+      cursor.setItalic(pixel.italic)
+      cursor.setUnderline(pixel.underline)
+      cursor.setBlinking(pixel.blinking)
+      cursor.setInverse(pixel.inverse)
+      cursor.setHidden(pixel.hidden)
+      cursor.setStrikethrough(pixel.strikethrough)
+      cursor.write(pixel.content)
+    }
   }
 }
