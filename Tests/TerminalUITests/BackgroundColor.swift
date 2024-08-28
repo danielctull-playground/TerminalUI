b3a57adf7f3c2a72ba9a1fd2824c1370b2c01a10
Daniel Tull <dt@danieltull.co.uk>
2024-08-28 10:44:14 +0200

    Add test for the backgroundColor modifier

---
+++ Tests/TerminalUITests/BackgroundColor.swift
@@ -0,0 +1,18 @@
+@testable import TerminalUI
+import TerminalUITesting
+import Testing
+
+@Suite("BackgroundColor Tests")
+struct BackgroundColorTests {
+
+  @Test("backgroundColor")
+  func backgroundColor() {
+
+    let text = Text("x")
+      .backgroundColor(.blue)
+
+    text.expect([
+      Position(x: 1, y: 0): Pixel("x", background: .blue),
+    ])
+  }
+}
