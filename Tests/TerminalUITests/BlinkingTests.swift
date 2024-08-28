73a832402b6202f50bc6f2374908275a85d688b7
Daniel Tull <dt@danieltull.co.uk>
2024-08-28 10:44:19 +0200

    Add test for the blinking modifier

---
+++ Tests/TerminalUITests/BlinkingTests.swift
@@ -0,0 +1,29 @@
+@testable import TerminalUI
+import TerminalUITesting
+import Testing
+
+@Suite("Blinking Tests")
+struct BlinkingTests {
+
+  @Test("blinking = on")
+  func on() {
+
+    let text = Text("x")
+      .blinking(true)
+
+    text.expect([
+      Position(x: 1, y: 0): Pixel("x", blinking: .on),
+    ])
+  }
+
+  @Test("blinking = off")
+  func off() {
+
+    let text = Text("x")
+      .blinking(false)
+
+    text.expect([
+      Position(x: 1, y: 0): Pixel("x", blinking: .off),
+    ])
+  }
+}
