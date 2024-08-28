1204041880fe5f2cfab1acc9f0e5bb62a9b85e24
Daniel Tull <dt@danieltull.co.uk>
2024-08-28 10:44:42 +0200

    Add test for the foregroundColor modifier

---
+++ Tests/TerminalUITests/ForegroundColorTests.swift
@@ -0,0 +1,18 @@
+@testable import TerminalUI
+import TerminalUITesting
+import Testing
+
+@Suite("ForegroundColor Tests")
+struct ForegroundColorTests {
+
+  @Test("foregroundColor")
+  func foregroundColor() {
+
+    let text = Text("x")
+      .foregroundColor(.blue)
+
+    text.expect([
+      Position(x: 1, y: 0): Pixel("x", foreground: .blue),
+    ])
+  }
+}
