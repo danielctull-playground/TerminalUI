5281873801c42a4d933d42345e5699e5299a7a10
Daniel Tull <dt@danieltull.co.uk>
2024-08-28 10:47:37 +0200

    Add test for the italic modifier

---
+++ Tests/TerminalUITests/ItalicTests.swift
@@ -0,0 +1,29 @@
+@testable import TerminalUI
+import TerminalUITesting
+import Testing
+
+@Suite("Italic Tests")
+struct ItalicTests {
+
+  @Test("italic = on")
+  func on() {
+
+    let text = Text("x")
+      .italic(true)
+
+    text.expect([
+      Position(x: 1, y: 0): Pixel("x", italic: .on),
+    ])
+  }
+
+  @Test("italic = off")
+  func off() {
+
+    let text = Text("x")
+      .italic(false)
+
+    text.expect([
+      Position(x: 1, y: 0): Pixel("x", italic: .off),
+    ])
+  }
+}
