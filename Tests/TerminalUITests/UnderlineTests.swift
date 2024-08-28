7391fec6216872f2c04cb1b345ceef21ebca209d
Daniel Tull <dt@danieltull.co.uk>
2024-08-28 10:49:10 +0200

    Add test for the underline modifier

---
+++ Tests/TerminalUITests/UnderlineTests.swift
@@ -0,0 +1,29 @@
+@testable import TerminalUI
+import TerminalUITesting
+import Testing
+
+@Suite("Underline Tests")
+struct UnderlineTests {
+
+  @Test("underline = on")
+  func on() {
+
+    let text = Text("x")
+      .underline(true)
+
+    text.expect([
+      Position(x: 1, y: 0): Pixel("x", underline: .on),
+    ])
+  }
+
+  @Test("underline = off")
+  func off() {
+
+    let text = Text("x")
+      .underline(false)
+
+    text.expect([
+      Position(x: 1, y: 0): Pixel("x", underline: .off),
+    ])
+  }
+}
