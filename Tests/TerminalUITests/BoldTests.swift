40243b8408b737572bdebe52431d859d0c6a9d9e
Daniel Tull <dt@danieltull.co.uk>
2024-08-28 10:44:25 +0200

    Add test for the bold modifier

---
+++ Tests/TerminalUITests/BoldTests.swift
@@ -0,0 +1,29 @@
+@testable import TerminalUI
+import TerminalUITesting
+import Testing
+
+@Suite("Bold Tests")
+struct BoldTests {
+
+  @Test("bold = on")
+  func on() {
+
+    let text = Text("x")
+      .bold(true)
+
+    text.expect([
+      Position(x: 1, y: 0): Pixel("x", bold: .on),
+    ])
+  }
+
+  @Test("bold = off")
+  func off() {
+
+    let text = Text("x")
+      .bold(false)
+
+    text.expect([
+      Position(x: 1, y: 0): Pixel("x", bold : .off),
+    ])
+  }
+}
