422374c81f9e55c15598287ce654f2c6e696a2dd
Daniel Tull <dt@danieltull.co.uk>
2024-10-24 19:07:50 +0100

    Update to Swift 6

--- Sources/TerminalUI/Color.swift
+++ Sources/TerminalUI/Color.swift
@@ -7,15 +7,15 @@ public struct Color: Equatable {
 // MARK: - ANSI colors
 
 extension Color {
-  public static let black = ansi(foreground: 30, background: 40)
-  public static let red = ansi(foreground: 31, background: 41)
-  public static let green = ansi(foreground: 32, background: 42)
-  public static let yellow = ansi(foreground: 33, background: 43)
-  public static let blue = ansi(foreground: 34, background: 44)
-  public static let magenta = ansi(foreground: 35, background: 45)
-  public static let cyan = ansi(foreground: 36, background: 46)
-  public static let white = ansi(foreground: 37, background: 47)
-  public static let `default` = ansi(foreground: 39, background: 49)
+  public static var black: Color { ansi(foreground: 30, background: 40) }
+  public static var red: Color { ansi(foreground: 31, background: 41) }
+  public static var green: Color { ansi(foreground: 32, background: 42) }
+  public static var yellow: Color { ansi(foreground: 33, background: 43) }
+  public static var blue: Color { ansi(foreground: 34, background: 44) }
+  public static var magenta: Color { ansi(foreground: 35, background: 45) }
+  public static var cyan: Color { ansi(foreground: 36, background: 46) }
+  public static var white: Color { ansi(foreground: 37, background: 47) }
+  public static var `default`: Color { ansi(foreground: 39, background: 49) }
 }
 
 extension Color {
