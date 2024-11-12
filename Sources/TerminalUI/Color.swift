3829f0dc7b1fb10d3b80d852bde6e951b08bca89
Daniel Tull <dt@danieltull.co.uk>
2024-11-12 17:41:49 +0000

    Add a description for colors
    This improves the output in the test navigator for the color tests.

--- Sources/TerminalUI/Color.swift
+++ Sources/TerminalUI/Color.swift
@@ -1,5 +1,6 @@
 
-public struct Color: Equatable, Sendable {
+public struct Color: CustomStringConvertible, Equatable, Sendable {
+  public let description: String
   let foreground: ControlSequence
   let background: ControlSequence
 }
@@ -7,23 +8,25 @@ public struct Color: Equatable, Sendable {
 // MARK: - ANSI colors
 
 extension Color {
-  public static var black: Color { ansi(foreground: 30, background: 40) }
-  public static var red: Color { ansi(foreground: 31, background: 41) }
-  public static var green: Color { ansi(foreground: 32, background: 42) }
-  public static var yellow: Color { ansi(foreground: 33, background: 43) }
-  public static var blue: Color { ansi(foreground: 34, background: 44) }
-  public static var magenta: Color { ansi(foreground: 35, background: 45) }
-  public static var cyan: Color { ansi(foreground: 36, background: 46) }
-  public static var white: Color { ansi(foreground: 37, background: 47) }
-  public static var `default`: Color { ansi(foreground: 39, background: 49) }
+  public static var black: Color { ansi(name: "black", foreground: 30, background: 40) }
+  public static var red: Color { ansi(name: "red", foreground: 31, background: 41) }
+  public static var green: Color { ansi(name: "green", foreground: 32, background: 42) }
+  public static var yellow: Color { ansi(name: "yellow", foreground: 33, background: 43) }
+  public static var blue: Color { ansi(name: "blue", foreground: 34, background: 44) }
+  public static var magenta: Color { ansi(name: "magenta", foreground: 35, background: 45) }
+  public static var cyan: Color { ansi(name: "cyan", foreground: 36, background: 46) }
+  public static var white: Color { ansi(name: "white", foreground: 37, background: 47) }
+  public static var `default`: Color { ansi(name: "default", foreground: 39, background: 49) }
 }
 
 extension Color {
   private static func ansi(
+    name: String,
     foreground: GraphicRendition,
     background: GraphicRendition
   ) -> Color {
     Color(
+      description: "ansi(\(name))",
       foreground: .selectGraphicRendition(foreground),
       background: .selectGraphicRendition(background))
   }
