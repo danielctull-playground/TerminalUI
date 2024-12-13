05ed0730838f2afb964175bc25f60b77252aac6b
Daniel Tull <dt@danieltull.co.uk>
2024-12-13 17:54:52 +0000

    Add size to the builtin render function

--- Sources/TerminalUI/Views/Text.swift
+++ Sources/TerminalUI/Views/Text.swift
@@ -7,7 +7,7 @@ public struct Text: Builtin, View {
     self.string = string
   }
 
-  func render(in canvas: any Canvas, environment: EnvironmentValues) {
+  func render(in canvas: any Canvas, size: Size, environment: EnvironmentValues) {
     for (character, index) in zip(string, 1...) {
       let pixel = Pixel(
         character,
--- Sources/TerminalUITesting/View.expect.swift
+++ Sources/TerminalUITesting/View.expect.swift
@@ -13,7 +13,7 @@ extension View {
 
   package func expect(_ expected: [Position: Pixel]) {
     let canvas = TestCanvas()
-    _render(in: canvas)
+    _render(in: canvas, size: Size(width: 10, height: 10))
     #expect(canvas.pixels == expected)
   }
 }
