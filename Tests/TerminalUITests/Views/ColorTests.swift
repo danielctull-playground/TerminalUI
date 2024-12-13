cd73be87f43fae059eec557b17d1bb9e110010bb
Daniel Tull <dt@danieltull.co.uk>
2024-12-13 18:05:41 +0000

    Conform Color to View

---
+++ Tests/TerminalUITests/Views/ColorTests.swift
@@ -0,0 +1,27 @@
+@testable import TerminalUI
+import TerminalUITesting
+import Testing
+
+@Suite("Color", .tags(.view))
+struct ColorTests {
+
+  @Test("Color displays correctly")
+  func display() {
+
+    let canvas = TestCanvas()
+
+    Color.red._render(in: canvas, size: Size(width: 3, height: 3))
+
+    #expect(canvas.pixels == [
+      Position(x: 1, y: 1): Pixel(" ", background: .red),
+      Position(x: 2, y: 1): Pixel(" ", background: .red),
+      Position(x: 3, y: 1): Pixel(" ", background: .red),
+      Position(x: 1, y: 2): Pixel(" ", background: .red),
+      Position(x: 2, y: 2): Pixel(" ", background: .red),
+      Position(x: 3, y: 2): Pixel(" ", background: .red),
+      Position(x: 1, y: 3): Pixel(" ", background: .red),
+      Position(x: 2, y: 3): Pixel(" ", background: .red),
+      Position(x: 3, y: 3): Pixel(" ", background: .red),
+    ])
+  }
+}
