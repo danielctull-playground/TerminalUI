33f8044cf6edd8fbd4bb10b6479e52bcf516617c
Daniel Tull <dt@danieltull.co.uk>
2024-12-17 15:21:32 +0000

    Add fixed frame modifier

---
+++ Tests/TerminalUITests/ViewModifiers/FixedFrameTests.swift
@@ -0,0 +1,120 @@
+import TerminalUI
+import TerminalUITesting
+import Testing
+
+@Suite("FixedFrame", .tags(.viewModifier))
+struct FixedFrameTests {
+
+  private let canvas = TestCanvas(width: 3, height: 3)
+  private let view = Color.blue
+  private let pixel = Pixel(" ", background: .blue)
+
+  @Test("width: nil, height: nil")
+  func widthNil_heightNil() {
+
+    canvas.render {
+      view.frame()
+    }
+
+    #expect(canvas.pixels == [
+      Position(x: 1, y: 1): pixel,
+      Position(x: 2, y: 1): pixel,
+      Position(x: 3, y: 1): pixel,
+      Position(x: 1, y: 2): pixel,
+      Position(x: 2, y: 2): pixel,
+      Position(x: 3, y: 2): pixel,
+      Position(x: 1, y: 3): pixel,
+      Position(x: 2, y: 3): pixel,
+      Position(x: 3, y: 3): pixel,
+    ])
+  }
+
+  @Test("width: 1, height: nil")
+  func width1_heightNil() {
+
+    canvas.render {
+      view.frame(width: 1)
+    }
+
+    #expect(canvas.pixels == [
+      Position(x: 2, y: 1): pixel,
+      Position(x: 2, y: 2): pixel,
+      Position(x: 2, y: 3): pixel,
+    ])
+  }
+
+  @Test("width: 2, height: nil")
+  func width2_heightNil() {
+
+    canvas.render {
+      view.frame(width: 2)
+    }
+
+    #expect(canvas.pixels == [
+      Position(x: 1, y: 1): pixel,
+      Position(x: 1, y: 2): pixel,
+      Position(x: 1, y: 3): pixel,
+      Position(x: 2, y: 1): pixel,
+      Position(x: 2, y: 2): pixel,
+      Position(x: 2, y: 3): pixel,
+    ])
+  }
+
+  @Test("width: nil, height: 1")
+  func widthNil_height1() {
+
+    canvas.render {
+      view.frame(height: 1)
+    }
+
+    #expect(canvas.pixels == [
+      Position(x: 1, y: 2): pixel,
+      Position(x: 2, y: 2): pixel,
+      Position(x: 3, y: 2): pixel,
+    ])
+  }
+
+  @Test("width: nil, height: 2")
+  func widthNil_height2() {
+
+    canvas.render {
+      view.frame(height: 2)
+    }
+
+    #expect(canvas.pixels == [
+      Position(x: 1, y: 1): pixel,
+      Position(x: 2, y: 1): pixel,
+      Position(x: 3, y: 1): pixel,
+      Position(x: 1, y: 2): pixel,
+      Position(x: 2, y: 2): pixel,
+      Position(x: 3, y: 2): pixel,
+    ])
+  }
+
+  @Test("width: 1, height: 1")
+  func width1_height1() {
+
+    canvas.render {
+      view.frame(width: 1, height: 1)
+    }
+
+    #expect(canvas.pixels == [
+      Position(x: 2, y: 2): pixel,
+    ])
+  }
+
+  @Test("width: 2, height: 2")
+  func width2_height2() {
+
+    canvas.render {
+      view.frame(width: 2, height: 2)
+    }
+
+    #expect(canvas.pixels == [
+      Position(x: 1, y: 1): pixel,
+      Position(x: 2, y: 1): pixel,
+      Position(x: 1, y: 2): pixel,
+      Position(x: 2, y: 2): pixel,
+    ])
+  }
+}
