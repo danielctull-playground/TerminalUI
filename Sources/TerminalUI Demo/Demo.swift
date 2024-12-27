4fb3a8ee923cf9730c4a694c71f9e90023088b4d
Daniel Tull <dt@danieltull.co.uk>
2024-12-27 16:56:08 +0000

    Demo (to undo)

--- Sources/TerminalUI Demo/Demo.swift
+++ Sources/TerminalUI Demo/Demo.swift
@@ -4,11 +4,23 @@ import TerminalUI
 struct Demo: App {
 
   var body: some View {
-    Text("daniel")
-      .bold()
-      .italic()
-      .underline()
-      .blinking()
-      .inverse()
+
+    HStack(children: [
+      Color.red.padding(.leading, 1),
+      Color.green.padding(.leading, 1),
+      Color.yellow.padding(.leading, 1),
+      Color.blue.padding(.leading, 1),
+      Color.magenta.padding(.leading, 1),
+      Color.cyan.padding(.leading, 1),
+      Color.white.padding(.leading, 1),
+    ])
+    .frame(height: 2)
+
+//    Text("daniel")
+//      .bold()
+//      .italic()
+//      .underline()
+//      .blinking()
+//      .inverse()
   }
 }
