fade10d3df1135a58a2099639906570b87d7c195
Daniel Tull <dt@danieltull.co.uk>
2025-01-07 15:33:53 +0000

    Add support for spacing in HStack

--- Sources/TerminalUI/Layouts/HStack.swift
+++ Sources/TerminalUI/Layouts/HStack.swift
@@ -4,15 +4,22 @@ public struct HStack {
   @Mutable private var sizes: [Size] = []
 
   private let alignment: VerticalAlignment
+  private let _spacing: Int?
   private let content: [any View]
 
   public init(
     alignment: VerticalAlignment = .center,
+    spacing: Int? = nil,
     content: [any View]
   ) {
     self.alignment = alignment
+    self._spacing = spacing
     self.content = content
   }
+}
+
+extension HStack {
+  fileprivate var spacing: Int { _spacing ?? 0 }
 }
 
 extension HStack: Builtin, View {
@@ -35,8 +42,9 @@ extension HStack: Builtin, View {
       .map(\.1)
 
     sizes = Array(repeating: .zero, count: children.count)
+    let totalSpacing = spacing * (content.count - 1)
     var remainingChildren = children.count
-    var remainingWidth = proposal.width
+    var remainingWidth = proposal.width - totalSpacing
 
     for (index, child) in children {
 
@@ -51,7 +59,7 @@ extension HStack: Builtin, View {
       remainingWidth -= size.width
     }
 
-    let width = sizes.map(\.width).reduce(0, +)
+    let width = sizes.map(\.width).reduce(0, +) + totalSpacing
     let height = sizes.map(\.height).max() ?? 0
     return Size(width: width, height: height)
   }
@@ -66,7 +74,7 @@ extension HStack: Builtin, View {
       let y = alignment.value(in: size) - alignment.value(in: childSize)
       let canvas = canvas.translateBy(x: offset, y: y)
       child._render(in: canvas, size: childSize, environment: environment)
-      offset += childSize.width
+      offset += childSize.width + spacing
     }
   }
 }
