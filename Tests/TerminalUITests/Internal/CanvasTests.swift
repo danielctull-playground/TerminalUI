0eed0bd9d2d730036c6a57e2644920c5af321c09
Daniel Tull <dt@danieltull.co.uk>
2025-01-07 18:46:50 +0000

    Move test with the rest of the Canvas tests

--- Tests/TerminalUITests/Internal/CanvasTests.swift
+++ Tests/TerminalUITests/Internal/CanvasTests.swift
@@ -49,4 +49,15 @@ struct CanvasTests {
       Position(x: 2, y: 2): pixel,
     ])
   }
+
+  @Test("Center alignment")
+  func center() {
+    let canvas = TestCanvas(width: 3, height: 3)
+    canvas.render {
+      Text("A")
+    }
+    #expect(canvas.pixels == [
+      Position(x: 2, y: 2): Pixel("A")
+    ])
+  }
 }
