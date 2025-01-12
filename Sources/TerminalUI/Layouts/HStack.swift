2e6a2a5787674ea5d6b33c7635265c174d902fd9
Daniel Tull <dt@danieltull.co.uk>
2025-01-12 13:08:21 +0000

    Add implementation of HStack Layout

--- Sources/TerminalUI/Layouts/HStack.swift
+++ Sources/TerminalUI/Layouts/HStack.swift
@@ -22,6 +22,67 @@ extension HStack {
   fileprivate var spacing: Int { _spacing ?? 0 }
 }
 
+extension HStack: Layout {
+
+  public func makeCache(subviews: Subviews) -> [Size] {
+    Array(repeating: .zero, count: subviews.count)
+  }
+
+  public func sizeThatFits(
+    proposal: ProposedViewSize,
+    subviews: Subviews,
+    cache sizes: inout [Size]
+  ) -> Size {
+
+    let flexibilities = subviews.map { subview in
+      let min = ProposedViewSize(width: 0, height: proposal.height)
+      let max = ProposedViewSize(width: .max, height: proposal.height)
+      let smallest = subview.sizeThatFits(min)
+      let largest = subview.sizeThatFits(max)
+      return largest.width - smallest.width
+    }
+
+    let subviews = zip(flexibilities, subviews.enumerated())
+      .sorted(by: \.0)
+      .map(\.1)
+
+    let totalSpacing = spacing * (subviews.count - 1)
+    var remainingChildren = subviews.count
+    var remainingWidth = proposal.replacingUnspecifiedDimensions().width - totalSpacing
+
+    for (index, subview) in subviews {
+
+      let proposal = ProposedViewSize(
+        width: remainingWidth / remainingChildren,
+        height: proposal.height)
+
+      let size = subview.sizeThatFits(proposal)
+      sizes[index] = size
+      remainingChildren -= 1
+      remainingWidth -= size.width
+    }
+
+    let width = sizes.map(\.width).reduce(0, +) + totalSpacing
+    let height = sizes.map(\.height).max() ?? 0
+    return Size(width: width, height: height)
+  }
+
+  public func placeSubviews(
+    in bounds: Rect,
+    proposal: ProposedViewSize,
+    subviews: Subviews,
+    cache sizes: inout [Size]
+  ) {
+    var offset = 0
+    for (subview, size) in zip(subviews, sizes) {
+      let y = alignment.value(in: bounds.size) - alignment.value(in: size)
+      let position = Position(x: offset, y: y)
+      subview.place(at: position, proposal: ProposedViewSize(size))
+      offset += size.width + spacing
+    }
+  }
+}
+
 extension HStack: Builtin, View {
 
   func size(
