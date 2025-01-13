a6ec822ce096a181fa03d3047d149338a50b425d
Daniel Tull <dt@danieltull.co.uk>
2025-01-13 13:52:30 +0000

    Fix Size tests

--- Tests/TerminalUITests/Primitives/SizeTests.swift
+++ Tests/TerminalUITests/Primitives/SizeTests.swift
@@ -1,10 +1,10 @@
-@testable import TerminalUI
+import TerminalUI
 import Testing
 
 @Suite("Size") struct SizeTests {
 
   @Test("init(width:height:)")
-  func `init`() {
+  func init_width_height() {
     let size = Size(width: 10, height: 8)
     #expect(size.width == 10)
     #expect(size.height == 8)
