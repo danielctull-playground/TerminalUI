1f55fc849c7cff4de5c315c3d046dc0ce9ddfcf6
Daniel Tull <dt@danieltull.co.uk>
2025-01-14 22:08:12 +0000

    Add rule nodes to attribute graph

--- Sources/TerminalUI/Internal/AttributeGraph.swift
+++ Sources/TerminalUI/Internal/AttributeGraph.swift
@@ -1,3 +1,60 @@
 
+// MARK: - AttributeGraph
+
 package final class AttributeGraph {
+
+  package init() {}
+
+  package func rule<Value>(
+    _ name: Node.Name,
+    makeValue: @escaping () -> Value
+  ) -> Node<Value> {
+    Node(name: name, makeValue: makeValue)
+  }
 }
+
+// MARK: - Node
+
+extension AttributeGraph {
+
+  package final class Node<Value>: AnyNode {
+
+    package let name: Name
+    private let makeValue: () -> Value
+
+    package var value: Value {
+      makeValue()
+    }
+
+    fileprivate init(name: Name, makeValue: @escaping () -> Value) {
+      self.name = name
+      self.makeValue = makeValue
+    }
+  }
+}
+
+// MARK: - Node.Name
+
+extension AttributeGraph.Node {
+  package typealias Name = AttributeGraph.NodeName
+}
+
+extension AttributeGraph {
+  package struct NodeName: Equatable {
+    private let raw: String
+  }
+}
+
+extension AttributeGraph.Node.Name: ExpressibleByStringLiteral {
+  package init(stringLiteral value: String) {
+    self.init(raw: value)
+  }
+}
+
+extension AttributeGraph.Node.Name: CustomStringConvertible {
+  package var description: String { raw }
+}
+
+// MARK: - AnyNode
+
+private protocol AnyNode {}
--- Tests/TerminalUITests/Internal/AttributeGraphTests.swift
+++ Tests/TerminalUITests/Internal/AttributeGraphTests.swift
@@ -3,4 +3,13 @@ import Testing
 
 @Suite("AttributeGraph")
 struct AttributeGraphTests {
+
+  @Test("rule")
+  func rule() {
+    let graph = AttributeGraph()
+    let node = graph.rule("value") { 12 }
+    #expect(node.value == 12)
+    #expect(node.name == "value")
+    #expect(node.name.description == "value")
+  }
 }
