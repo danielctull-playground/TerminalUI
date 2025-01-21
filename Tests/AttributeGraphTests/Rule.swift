167e1ebec1e1cba001350519ed2ece17fb591f1f
Daniel Tull <dt@danieltull.co.uk>
2025-01-21 17:44:49 +0000

    Add rule

---
+++ Tests/AttributeGraphTests/Rule.swift
@@ -0,0 +1,25 @@
+import AttributeGraph
+import Testing
+
+@Suite("Rule")
+struct RuleTests {
+
+  @Test("create")
+  func create() {
+    let graph = Graph()
+    let rule = graph.rule("rule") { 12 }
+    #expect(rule.value == 12)
+    #expect(rule.name == "rule")
+    #expect(rule.name.description == "rule")
+  }
+
+  @Test("update")
+  func update() {
+    let graph = Graph()
+    let x = graph.attribute("x", value: 1)
+    let y = graph.rule("y") { x.value * 2 }
+    #expect(y.value == 2)
+    x.value = 2
+    #expect(y.value == 4)
+  }
+}
