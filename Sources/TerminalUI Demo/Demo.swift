4383b52929299e5a39a944c223341c9cde258b8c
Daniel Tull <dt@danieltull.co.uk>
2025-07-27 18:55:18 +0100

    Increase complexity of the demo

--- Sources/TerminalUI Demo/Demo.swift
+++ Sources/TerminalUI Demo/Demo.swift
@@ -4,11 +4,22 @@ import TerminalUI
 struct Demo: App {
 
   var body: some View {
-    Text("daniel")
-      .bold()
-      .italic()
-      .underline()
-      .blinking()
-      .inverse()
+    HStack {
+      Color(red: 1, green: 0, blue: 0)
+      VStack {
+        Color(red: 0.5, green: 0, blue: 1)
+        HStack {
+          Text("daniel")
+            .italic()
+            .padding(1)
+          Text("tull")
+            .underline()
+            .padding(.trailing, 1)
+        }
+        Color(red: 1, green: 1, blue: 0)
+      }
+      Color(red: 1, green: 0.5, blue: 0)
+    }
+    .bold()
   }
 }
