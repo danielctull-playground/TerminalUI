9b60f51ca3ae78c4fb62448121a60b2fa0e306bb
Daniel Tull <dt@danieltull.co.uk>
2025-08-06 23:24:35 +0100

    Allow the tests to run on CI
    Store the window size in a global property which can be used when retrieving the window size fails, as happens on CI.

--- Sources/TerminalUI/Rendering/Environment Values/WindowBounds.swift
+++ Sources/TerminalUI/Rendering/Environment Values/WindowBounds.swift
@@ -11,19 +11,28 @@ private struct WindowBoundsEnvironmentKey: EnvironmentKey {
   static var defaultValue: Rect { Rect(origin: .origin, size: .zero) }
 }
 
+@MainActor
+/// Stores the window size for when getting the window size fails.
+///
+/// Note: This happens on CI, so this allows the tests to pass there.
+private var windowSize = Size.zero
+
 extension Size {
 
   /// The size of the window of a terminal device.
+  @MainActor
   static var window: Size {
     get {
       var size = winsize()
-      _ = ioctl(STDOUT_FILENO, UInt(TIOCGWINSZ), &size)
+      let result = ioctl(STDOUT_FILENO, UInt(TIOCGWINSZ), &size)
+      guard result == EXIT_SUCCESS else { return windowSize }
       return Size(
         width: Int(size.ws_col),
         height: Int(size.ws_row)
       )
     }
     set {
+      windowSize = newValue
       var size = winsize()
       size.ws_col = UInt16(newValue.width)
       size.ws_row = UInt16(newValue.height)
