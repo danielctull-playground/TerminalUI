932a2aaee56476d08528882913f42e89a482030f
Daniel Tull <dt@danieltull.co.uk>
2025-08-12 17:42:00 +0100

    Add view builder tag

--- Tests/TerminalUITests/Metadata/Tags.swift
+++ Tests/TerminalUITests/Metadata/Tags.swift
@@ -2,6 +2,7 @@ import Testing
 
 extension Tag {
   @Tag static var view: Self
+  @Tag static var viewBuilder: Self
   @Tag static var modifier: Self
   @Tag static var layout: Self
   @Tag static var preferences: Self
