54e95c13efb9c0701cf90f4e2d48d75b87a09eed
Daniel Tull <dt@danieltull.co.uk>
2026-03-20 09:28:02 +0000

    Read all available byes
    If we fill up the buffer, continue reading until the count is less than the buffer size.

--- Sources/TerminalUI/Events/Support/StandardInput.swift
+++ Sources/TerminalUI/Events/Support/StandardInput.swift
@@ -13,12 +13,21 @@ struct StandardInput: AsyncSequence, Sendable {
   func makeAsyncIterator() -> AsyncMapSequence<AsyncStream<Void>, [Byte]>.Iterator {
     AsyncStream(DispatchSource.makeReadSource(fileDescriptor: STDIN_FILENO))
       .map {
+
+        var bytes: [Byte] = []
+
         let size = 64
         var buffer: [UInt8] = Array(repeating: 0, count: size)
-        let readCount = read(STDIN_FILENO, &buffer, buffer.count)
-        return buffer
-          .prefix(readCount)
-          .map(Byte.init(_:))
+        var count: Int
+
+        // Repeat if we fill up the buffer, to read all available byes.
+        repeat {
+          count = read(STDIN_FILENO, &buffer, size)
+          guard count > 0 else { break }
+          bytes.append(contentsOf: buffer.prefix(count).map(Byte.init(_:)))
+        } while count == size
+
+        return bytes
       }
       .makeAsyncIterator()
   }
