a85b9d5262bf82601a159f9acc956d0ba4dc7691
Daniel Tull <dt@danieltull.co.uk>
2026-07-09 06:56:40 +0100

    Add a function to rasterize a display list into a canvas

---
+++ Sources/TerminalUI/Engine/Render/Rasterize.swift
@@ -0,0 +1,35 @@
+
+extension Canvas {
+  
+  /// Rasterizes a list of items to the canvas.
+  ///
+  /// - Parameter displayList: The list of items to rasterize.
+  func rasterize(_ displayList: DisplayList) {
+    for item in displayList.items {
+      switch item.content {
+      case let .fill(style): fill(in: item.frame, with: style)
+      case let .text(text, style): draw(text: text, in: item.frame, with: style)
+      }
+    }
+  }
+
+  private func fill(in frame: Rect, with style: Style) {
+    guard frame.size.width > 0, frame.size.height > 0 else { return }
+    for x in frame.minX...frame.maxX {
+      for y in frame.minY...frame.maxY {
+        draw(Pixel(content: " ", graphicRendition: style.rendition), at: Position(x: x, y: y))
+      }
+    }
+  }
+
+  private func draw(text: String, in frame: Rect, with style: Style) {
+    guard frame.size.width > 0, frame.size.height > 0 else { return }
+    let lines = text.lines(ofLength: frame.size.width)
+    for (line, y) in zip(lines, frame.origin.y...) {
+      for (character, x) in zip(line, frame.origin.x...) {
+        let pixel = Pixel(content: character, graphicRendition: style.rendition)
+        draw(pixel, at: Position(x: x, y: y))
+      }
+    }
+  }
+}
--- Sources/TerminalUI/Views/Text.swift
+++ Sources/TerminalUI/Views/Text.swift
@@ -66,31 +66,3 @@ public struct Text: PrimitiveView {
     }
   }
 }
-
-extension StringProtocol {
-
-  fileprivate func lines(ofLength lineLength: Int) -> [SubSequence] {
-
-    guard lineLength > 0 else { return [] }
-    guard count > lineLength else {
-      return [self[startIndex..<endIndex]]
-    }
-
-    let head: SubSequence
-    let tail: SubSequence
-    if dropFirst(lineLength).first == " " {
-      let space = index(startIndex, offsetBy: lineLength)
-      head = self[..<space]
-      tail = self[index(after: space)...]
-    } else if let space = dropLast(count - lineLength).lastIndex(of: " ") {
-      head = self[..<space]
-      tail = self[index(after: space)...]
-    } else {
-      let end = index(startIndex, offsetBy: lineLength)
-      head = self[..<end]
-      tail = self[end...]
-    }
-
-    return [head] + tail.lines(ofLength: lineLength)
-  }
-}
